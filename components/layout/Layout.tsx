6e850ed6afbb73c1a3e5545389099585489a0e6a
Nico Rubinstein <nicorubinstein1@gmail.com>
2022-08-28 17:27:36 -0300

    final5

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -5,29 +5,35 @@ import { ArrowBack } from "@mui/icons-material";
 import { useRouter } from "next/router";
 import AuthButton from "../authButton/AuthButton";
 import { useAuth } from "../../context/AuthContext";
+import Head from "next/head";
 
 const Layout = ({ children }: any) => {
   const router = useRouter();
   const isSignedIn = useAuth();
   return (
-    <div>
-      <header className={styles.header}>
-        <div className={styles.back}>
-          {router.asPath !== "/" && router.asPath !== "/app" && (
-            <IconButton onClick={() => router.back()}>
-              <ArrowBack />
-            </IconButton>
-          )}
-        </div>
-        <h1>Qatar collector 2022</h1>
-        <div className={styles.auth}>
-          <AuthButton isSignedIn={isSignedIn} />
-        </div>
+    <>
+      <Head>
+        <title>Qatar Collector</title>
+      </Head>
+      <div>
+        <header className={styles.header}>
+          <div className={styles.back}>
+            {router.asPath !== "/" && router.asPath !== "/app" && (
+              <IconButton onClick={() => router.back()}>
+                <ArrowBack />
+              </IconButton>
+            )}
+          </div>
+          <h1>Qatar collector 2022</h1>
+          <div className={styles.auth}>
+            <AuthButton isSignedIn={isSignedIn} />
+          </div>
 
-        <div></div>
-      </header>
-      {children}
-    </div>
+          <div></div>
+        </header>
+        {children}
+      </div>
+    </>
   );
 };
 
