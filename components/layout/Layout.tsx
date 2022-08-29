c87971ba1826c23f332eb32d4ed311a7da1d9d13
Nico Rubinstein <nicorubinstein1@gmail.com>
2022-08-29 14:51:45 -0300

    asdf

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,15 +1,19 @@
 import { IconButton } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./layout.module.scss";
-import { ArrowBack } from "@mui/icons-material";
+import { ArrowBack, Menu } from "@mui/icons-material";
 import { useRouter } from "next/router";
-import AuthButton from "../authButton/AuthButton";
-import { useAuth } from "../../context/AuthContext";
+
 import Head from "next/head";
+import Sidebar from "../sidebar/Sidebar";
 
 const Layout = ({ children }: any) => {
   const router = useRouter();
-  const isSignedIn = useAuth();
+  const [sidebarWidth, setSidebarWidth] = useState("0px");
+
+  const closeSidebar = () => {
+    setSidebarWidth("0px");
+  };
   return (
     <>
       <Head>
@@ -17,15 +21,16 @@ const Layout = ({ children }: any) => {
         <meta name="google" content="notranslate" />
         <link rel="icon" href="/icons/favicon.ico" type="image/x-icon" />
         <meta
-        name="description"
-        content={
-          "Crea álbumes, marca las figuritas que ya coleccionaste, y compartelos con familia y amigos en tiempo real."
-        }
-      />
-      <meta name="theme-color" content="#000000" />
-      <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
-      <link rel="manifest" href="/manifest.json" />
+          name="description"
+          content={
+            "Crea álbumes, marca las figuritas que ya coleccionaste, y compartelos con familia y amigos en tiempo real."
+          }
+        />
+        <meta name="theme-color" content="#000000" />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
       </Head>
+      {<Sidebar width={sidebarWidth} onClose={closeSidebar} />}
       <div>
         <header className={styles.header}>
           <div className={styles.back}>
@@ -37,15 +42,14 @@ const Layout = ({ children }: any) => {
           </div>
           <h1>Qatar collector 2022</h1>
           <div className={styles.auth}>
-            <AuthButton isSignedIn={isSignedIn} />
+            <IconButton onClick={() => setSidebarWidth("80vw")}>
+              <Menu />
+            </IconButton>
           </div>
 
           <div></div>
         </header>
         {children}
-        <div className={styles.authmob}>
-          <AuthButton isSignedIn={isSignedIn} />
-        </div>
       </div>
     </>
   );
---
+++ components/sidebar/Sidebar.tsx
@@ -0,0 +1,33 @@
+import { Close, Leaderboard } from "@mui/icons-material";
+import { Button, IconButton } from "@mui/material";
+import React from "react";
+import { useAuth } from "../../context/AuthContext";
+import AuthButton from "../authButton/AuthButton";
+import styles from "./sidebar.module.scss";
+
+const Sidebar = ({ onClose, width }: { onClose: any; width: string }) => {
+  const isSignedIn = useAuth();
+
+  return (
+    <div
+      className={styles.sidebar}
+      style={{ width, opacity: width === "0px" ? "0" : 1 }}
+    >
+      <div className={styles.close}>
+        <IconButton onClick={() => onClose()}>
+          <Close />
+        </IconButton>
+      </div>
+
+      <section className={styles.content}>
+        Próximamente
+        <Button className={styles.stats} startIcon={<Leaderboard />}>
+          Estadísticas
+        </Button>
+        <AuthButton isSignedIn={isSignedIn} />
+      </section>
+    </div>
+  );
+};
+
+export default Sidebar;
